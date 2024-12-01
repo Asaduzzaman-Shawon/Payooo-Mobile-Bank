d79519724ce552e9534b1b308ee9f43dee929dd4
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-02 01:05:42 +0600

    jacchi

--- js/js.js
+++ js/js.js
@@ -1,16 +1,12 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
+function check(){
+    const pin = document.getElementById('pin').value;
+    if(pin === "1234"){
+        window.location.href = 'home.html'  
+    }
+    else{
+        alert("Please enter the right ")
+    }
+}
 
 
 
